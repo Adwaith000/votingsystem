8eaa517a03d0f73c7cc90b1ef9a7497c5dde0496
Adwaith000 <adwaithas1010@gmail.com>
2025-05-15 11:19:34 +0530

    Fix: Switched from MySQL to PostgreSQL for Render deployment

--- e_voting_project/settings.py
+++ e_voting_project/settings.py
@@ -20,11 +20,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 STATIC_URL = '/static/'
@@ -109,16 +105,16 @@ WSGI_APPLICATION = 'e_voting_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'e_voting_db',
-        'USER': 'root',  # Or 'euser' if you created one
-        'PASSWORD': 'adwaith@sql123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'e_voting_db',
+        #'USER': 'root',  # Or 'euser' if you created one
+        #'PASSWORD': 'adwaith@sql123',
+        #'HOST': 'localhost',
+        #'PORT': '3306',
+    #}
+#}
 
 
 
